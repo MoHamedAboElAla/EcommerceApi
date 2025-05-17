f494fbdad9bc055a37306ce8566906a2ac19f39d
MohamedAboElAla <mohamed20300005@gmail.com>
2025-05-17 15:58:37 +0300

    Add Get Card Func

--- EcommerceApi/Controllers/CartController.cs
+++ EcommerceApi/Controllers/CartController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EcommerceApi.Models.DTO;
+using EcommerceApi.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CartController : ControllerBase
+    public class CartController(AppDbContext Context) : ControllerBase
     {
+        private readonly AppDbContext _context = Context;
+
+        [HttpGet]
+        public IActionResult GetCart(string productIdentifier)
+        {
+            CartDto cartDto = new CartDto();
+            cartDto.CartItems = new List<CartItemDto>();
+            cartDto.SubTotal = 0;
+            cartDto.ShippingFee = OrderHelper.ShippingFee;
+            cartDto.TotalPrice = 0;
+
+            var productDictionary = OrderHelper.GetProductDictionary(productIdentifier);
+
+            foreach (var product in productDictionary)
+            {
+                int productId = product.Key;
+                var productItem = _context.Products.FirstOrDefault(p => p.Id == productId);
+                if(productItem == null)
+                {
+                    continue;
+                }
+                var cartItem = new CartItemDto
+                {
+                    Product = productItem,
+                    Quantity = product.Value,
+
+                };
+                cartDto.CartItems.Add(cartItem);
+                cartDto.SubTotal += productItem.Price * product.Value;
+                cartDto.TotalPrice += cartDto.SubTotal + OrderHelper.ShippingFee;
+
+
+            }
+            return Ok(cartDto);
+        }
+        
     }
 }
