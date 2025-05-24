86ed88a6bc0872f7131c85a679e2b125112f1aeb
MohamedAboElAla <mohamed20300005@gmail.com>
2025-05-25 01:43:57 +0300

    Add DeleteOrderFunc

--- EcommerceApi/Controllers/OrdersController.cs
+++ EcommerceApi/Controllers/OrdersController.cs
@@ -39,15 +39,15 @@ namespace EcommerceApi.Controllers
             }
             query = query.OrderByDescending(i => i.Id);
 
-            if(page ==null || page < 1)
+            if (page == null || page < 1)
             {
                 page = 1;
             }
             int pageSize = 5; // Number of orders per page
             int totalPages = 0;
 
-           decimal count = query.Count(); // Get the total number of orders
-           totalPages = (int) Math.Ceiling(count / pageSize); // Calculate total pages
+            decimal count = query.Count(); // Get the total number of orders
+            totalPages = (int)Math.Ceiling(count / pageSize); // Calculate total pages
 
 
             query = query.Skip(((int)page - 1) * pageSize)
@@ -120,9 +120,9 @@ namespace EcommerceApi.Controllers
             return Ok(order);
         }
 
-         [Authorize]
+        [Authorize]
         [HttpPost]
-        public IActionResult Create(OrderDto orderDto) 
+        public IActionResult Create(OrderDto orderDto)
         {
             //1- Check if the Payment Mehtod is valid or not 
             if (!OrderHelper.PaymentMethods.ContainsKey(orderDto.PaymentMethod))
@@ -168,7 +168,7 @@ namespace EcommerceApi.Controllers
                 order.orderItems.Add(orderItem);
             }
 
-            if(order.orderItems.Count < 1)
+            if (order.orderItems.Count < 1)
             {
                 ModelState.AddModelError("Order", "No products found in the order");
                 return BadRequest(ModelState);
@@ -187,6 +187,23 @@ namespace EcommerceApi.Controllers
         }
 
 
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int? id)
+        {
+            var order = _context.Orders.Find(id);
+            if (order == null)
+            {
+                return NotFound($"Order with id {id} not found");
+            }
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+            return Ok(new
+            {
+                Message = $"Order with id {id} deleted successfully"
+
+            });
+        } 
 
     }
 }
